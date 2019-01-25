315e1c23c8ab5f6fa0e072114a14bff4dd13dab4
Philip Meier <philip.meier06@googlemail.com>
2019-01-25 14:51:43 +0000

    Update basic class examples

--- Classes101/basic_class.cpp
+++ Classes101/basic_class.cpp
@@ -8,33 +8,74 @@ namespace training
   class NumberHolder
   {
     public:
+      // default constructor
+      NumberHolder();
+
       // constructor
       NumberHolder(int number);
 
-      int getNumber();
+      int getNumber() const;
+
+      void setNumber(int number);
 
     private:
       int m_Number;
   };
+
+  NumberHolder::NumberHolder()
+  {
+    m_Number = 0;
+  }
 
   NumberHolder::NumberHolder(int number)
   {
     m_Number = number;
   }
 
-  int NumberHolder::getNumber()
+  int NumberHolder::getNumber() const
   {
     return m_Number;
   }
 
-  void basicClassUsage()
-  {
+  void NumberHolder::setNumber(int number) {
+    m_Number = number;
+  }
+
+
+  //////////////////////////////////////////
+
+
+  void instantiatingAndUsingAClass() {
     NumberHolder stackAllocated(4);
     cout << stackAllocated.getNumber() << endl;
 
     NumberHolder* heapAllocated = new NumberHolder(5);
     cout << heapAllocated->getNumber() << endl;
     delete heapAllocated;
+  }
+
+  void defaultConstruction() {
+    NumberHolder holder;
+    cout << holder.getNumber() << endl;
+  }
+
+  void constMethods() {
+    const NumberHolder holder(3);
+
+    cout << holder.getNumber() << endl;
+
+    // holder.setNumber(1);  // will not compile
+  }
+
+
+  //////////////////////////////////////////
+
+
+  void basicClassUsage()
+  {
+    cout << "**Basic class usage**" << endl;
+    instantiatingAndUsingAClass();
+    constMethods();
   }
 
 }
