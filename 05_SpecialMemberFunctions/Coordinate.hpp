fd2c14aea913c1dcd44ba907b3ab1c9a2bd61200
Philip Meier <philip.meier06@googlemail.com>
2019-01-27 21:22:12 +0000

    Fix coordinate example

--- 05_SpecialMemberFunctions/Coordinate.hpp
+++ 05_SpecialMemberFunctions/Coordinate.hpp
@@ -9,11 +9,6 @@ namespace training
      : m_X(x), m_Y(y)
     {}
 
-    bool operator==(const Coordinate& other) const
-    {
-      return (m_X == other.m_X && m_Y == other.m_Y);
-    }
-
     int m_X;
     int m_Y;
   };
